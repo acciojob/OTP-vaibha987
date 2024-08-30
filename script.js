4a26e0ec244578a96417697af3bcda29d42c81a5
vaibha987 <v2goyal95@gmail.com>
2024-08-30 18:41:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,19 @@
 let codes = document.querySelectorAll(".code")
 // change, input
-codes.forEach((code,index)=>{
+ codes.forEach((code,index)=>{
     
-    code.addEventListener("input",()=>{
-        if(code.value.length == 1){
-             if(index < codes.length-1){
-                   codes[index+1].focus()
-                   codes[index+1].classList.add("focused")
-             }
-        }
-    })
-    // keyup, keydown(press)
-    code.addEventListener("keydown",(e)=>{
-        if(e.key == "Backspace"){
-              if(index > 0 && code.value.length == 0){
-                
-                codes[index-1].focus()
-                codes[index-1].classList.add("focused")
-              }
-        }
-    })
-    //code.addEventListener("blur", ()=>{
-      //    code.classList.remove("focused")
-    //})
-})
+    code.addEventListener("keydown",(event)=>{
+	if(event.key>="0" && event.key<="9"){
+		event.preventDefault();
+		code.value=event.key
+		if(codes[index+1])
+		codes[index+1].focus()
+	}
+		else if(event.key==="Backspace"){
+		code.value=""
+			if(codes[index-1]){
+		   codes[index-1].focus()	
+		}
+		}
+	})
+ })
