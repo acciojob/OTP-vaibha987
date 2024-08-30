92ab856acd91e2d76b2a352d8157a14d77dc8781
vaibha987 <v2goyal95@gmail.com>
2024-08-30 19:20:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,12 +7,14 @@ codes.forEach((code,index)=>{
             code.value = event.key
             if(codes[index+1]) {
                 codes.forEach(code => code.classList.remove("focused"));
+                setTimeout(() => codes[index + 1]?.focus(), 10);
                 codes[index+1].classList.add("focused")
             }
         } else if(event.key === "Backspace"){
             code.value = ""
             if(codes[index-1]) {
                 codes.forEach(code => code.classList.remove("focused"));
+                setTimeout(() => codes[index - 1]?.focus(), 10);
                 codes[index-1].classList.add("focused")
             }
         }
