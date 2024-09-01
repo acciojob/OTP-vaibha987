193402c9e52d029b0bd8364c642976564d4bb441
vaibha987 <v2goyal95@gmail.com>
2024-09-01 15:13:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,16 +4,13 @@ codes[0].focus()
  
 codes.forEach((code,idx)=>{
     code.addEventListener("keydown",(e)=>{
-        console.log(e.key)
         const keyPressed = e.key
  
         if(keyPressed>=0 && keyPressed<=9){
             codes[idx].value=''
             if(idx<codes.length-1){
                 setTimeout(()=>codes[idx+1].focus(),10)
-                // setTimeout(function(){
-                //     codes[index+1].focus()
-                // },10)
+            
             }
         }
         else if(keyPressed==="Backspace"){
